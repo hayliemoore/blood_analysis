64ddcf0090b0af09e23637cf8e116bdb3daf748b
haylie moore <haylie.moore@duke.edu>
2020-01-24 12:54:44 -0500

    add the command to determine LDL

--- chol_analysis.py
+++ chol_analysis.py
@@ -6,13 +6,31 @@ def HDL_analysis(HDL_level):
     else:
         return "Low"
 
+def LDL_analysis(LDL_level):
+    if LDL_level < 130:
+        return "Normal"
+    elif 130 <= LDL_level < 159:
+        return "borderline high"
+    elif 160 <= LDL_level < 189:
+        return "high"
+    else:
+        return "very high"
+
+
+
+
 def cholesterol_analysis():
     print("Cholesterol Analysis")
     HDLinput = input("Enter test results: ")
     test_info = HDLinput.split("=")
-    if test_info[0] =="HDL":
+    if test_info[0] == "HDL":
         answer = HDL_analysis(int(test_info[1]))
         print("the level is {}".format(answer))
+    elif test_info[0] == "LDL":
+        answer = LDL_analysis(int(test_info[1]))
+        print("The level is {}".format(answer))
+    else:
+        print("test not recognized")
 
 
 
