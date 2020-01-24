fa8144134eef9c879dd14281100231ff8e401035
haylie moore <haylie.moore@duke.edu>
2020-01-24 12:12:02 -0500

    built basic outline for interface

---
+++ chol_analysis.py
@@ -0,0 +1,13 @@
+def interface():
+    print("cholestterol calculator")
+    print("options: ")
+    print(" 9 - Quit")
+    choice = input("Enter your option: ")
+    if choice == '9':
+        return
+       
+       
+       
+if __name__ == "__main__":
+    interface()
+    
