7386114d71b4b5b76c4193b5232db6ded7f0a011
haylie moore <haylie.moore@duke.edu>
2020-02-06 17:49:25 -0500

    testing how travis works by adding that file and made a new test in chol_analysis

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_HDL_analysis_normal():
     from chol_analysis import HDL_analysis
     answer = HDL_analysis(80)
@@ -24,4 +27,51 @@ def test_fever_check():
     new_data = [96.0, 100.5, 105.1, 97]
     answer = fever_check(new_data)
     expected = True
+    assert answer == expected
+
+
+def test_HDL_analysis():
+    """Tests HDL_analysis method
+    Args:
+        HDL_level
+    Returns:
+        Passed if answer == expected
+    """
+    from chol_analysis import HDL_analysis
+    input = 100
+    answer = HDL_analysis(input)
+    expected = 'Normal'
+    assert answer == expected
+
+
+def test_LDL_analysis():
+    """Tests LDL_analysis method
+    Args:
+        LDL_level
+    Returns:
+        Passed if answer == expected
+    """
+    from chol_analysis import LDL_analysis
+    input = 100
+    answer = LDL_analysis(input)
+    expected = 'Normal'
+    assert answer == expected
+
+
+@pytest.mark.parametrize("i, o", [
+    (100, 'Normal'),
+    (50, 'Borderline Low'),
+    (20, 'Low'),
+    (55.5, 'Borderline Low'),
+])
+def test_HDL_analysis_parametrize(i, o):
+    """Parametrize Tests HDL_analysis method
+    Args:
+        HDL_level
+    Returns:
+        Passed if answer == expected
+    """
+    from chol_analysis import HDL_analysis
+    answer = HDL_analysis(i)
+    expected = o
     assert answer == expected
